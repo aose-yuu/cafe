07a7e4a4ed5dbe9d551d46e6add319ad6c9393a4
aose-yuu <aose.developer@gmail.com>
2024-09-26 15:37:41 +0900

    fix: 🐛 Remove unwanted properties

--- custom-jiti.cjs
+++ custom-jiti.cjs
@@ -3,9 +3,6 @@ const alias = require("./alias.config.cjs").alias;
 const [filePath, ...args] = process.argv.slice(2);
 process.argv = [process.argv[0], filePath, ...args];
 
-const jiti = require("jiti")(__filename, {
-  esmRequire: true,
-  alias,
-});
+const jiti = require("jiti")(__filename, { alias });
 
 jiti(filePath);
